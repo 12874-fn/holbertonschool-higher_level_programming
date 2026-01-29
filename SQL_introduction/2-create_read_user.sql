f00a112764f0a56a1af2d85c7a93b8b345e511c7
12874-fn <12874@holbertonstudents.com>
2026-01-29 07:29:42 -0600

    Update 2-create_read_user.sql

--- SQL_introduction/2-create_read_user.sql
+++ SQL_introduction/2-create_read_user.sql
@@ -1,15 +1,11 @@
--- Task 2: Create database hbtn_0d_2 and read-only user user_0d_2
+-- Crea base de datos hbth_0d_2 y user_0d_2
 
--- Create database if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
--- Create user if it does not exist
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
 
--- Grant SELECT privilege only on hbtn_0d_2
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 
--- Apply privileges
 FLUSH PRIVILEGES;
 
